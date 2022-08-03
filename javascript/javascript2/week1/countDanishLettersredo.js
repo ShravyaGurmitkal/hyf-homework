8a2349a7953ecc1586c574764c582642223d0993
ShravyaGurmitkal <17876083+ShravyaGurmitkal@users.noreply.github.com>
2022-08-03 18:26:01 +0200

    Update javascript/javascript2/week1/countDanishLettersredo.js
    Co-authored-by: Milton Hultgren <miltonhultgren@gmail.com>

--- javascript/javascript2/week1/countDanishLettersredo.js
+++ javascript/javascript2/week1/countDanishLettersredo.js
@@ -5,15 +5,18 @@ const danishString2 = "Blå grød med røde bær";
 getCountOfDanishLetters(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
 
 function getCountOfDanishLetters (string) {
-    const danishLettersArray = string.match(/[æåøÆÅØ]/g);
-    let returnObj = {
-        total: danishLettersArray.length,
-    }
+  const danishLetters = string.match(/[æåøÆÅØ]/g);
+  
+  const reducer = (acc, letter) => {
+    const count = acc[letter] || 0;
+    return {
+      ...acc,
+      [letter]: count + 1,
+    };
+  };
 
-    danishLettersArray.forEach(letter => {
-        (returnObj[letter] === undefined) ? returnObj[letter] = 1 : returnObj[letter] += 1;
-    });
-
-    console.log(returnObj)
+  return danishLetters.reduce(reducer, {
+        total: danishLetters.length,
+  });
 }
 
