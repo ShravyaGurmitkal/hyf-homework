8651ea19175e010f4b93a01a4c3fd1802e6545ec
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-08-03 14:41:55 +0200

    this function will return a promise that resolves after the resolveAfter seconds has passed

---
+++ javascript/javascript3/week2/setTime.js
@@ -0,0 +1,23 @@
+//Calling this function will return a promise that resolves after the resolveAfter seconds has passed
+function setTime(resolveAfter) {
+    return new Promise((resolve, reject) => {
+      if(typeof resolveAfter !== "number") {
+        reject(`Time passed should be a number`);
+    }
+      setTimeout(resolve, resolveAfter * 1000)
+    });  
+}
+
+//using it with async/await
+async function asyncSetTime () {
+  await setTime(4).then(() => console.log("I am called asynchronously"))
+        .catch((error) => console.log(error));
+  console.log('This logs after the promise is resolved as we used await')
+}
+
+setTime(2).then(() => {
+    console.log("I am called asynchronously"); // logged out after 8 seconds
+  })
+  .catch((error) => console.log(error));
+
+asyncSetTime();
