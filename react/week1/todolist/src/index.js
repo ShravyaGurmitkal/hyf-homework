6959c3446a52485e5686e3648d8dd944f293c579
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-10-07 06:02:12 +0200

    index.js , changed as suggested in the review

--- react/week1/todolist/src/index.js
+++ react/week1/todolist/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TodoApp from './TodoApp'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoApp />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
