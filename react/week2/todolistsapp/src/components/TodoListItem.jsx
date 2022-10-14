a8e90dbb430bb929063f316ab691bf85253f467f
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-10-14 14:42:42 +0200

    changed the checkbox and textdecoration functionality

--- react/week2/todolistsapp/src/components/TodoListItem.jsx
+++ react/week2/todolistsapp/src/components/TodoListItem.jsx
@@ -1,21 +1,13 @@
+import { useState } from "react";
+
 export default function TodoListItem (props) {
     const {todo, id, deleteTodo} = props;
+    const [checked, setChecked] = useState(false);
 
-    function markTodo(todoId) {
-        const labels = document.querySelectorAll('label');
-        
-        for (const label of labels) {
-            if(Number(label.id) === todoId)
-                (label.children.item(0).checked) ?  label.style.textDecoration = "line-through" :  label.style.textDecoration = "none"
-        }
-    }
-    
     return (
         <li style={{margin: "10px"}}>
-            <label id={id}>
-                {todo.description}
-                <input type="checkbox" onChange={() => markTodo(id)}/>
-            </label>
+            <span style={{textDecoration: checked === true ? "line-through" : ""}}>{todo.description}</span>
+            <input type="checkbox" onChange={() => setChecked(!checked)}/>
             <button onClick={() => deleteTodo(id)} style={{fontSize: "18px", backgroundColor: "slategray"}}>Delete</button>
         </li>
     )
